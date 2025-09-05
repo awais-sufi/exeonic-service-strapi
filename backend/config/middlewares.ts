56183780d4d59a81c3b65c64722e238f75ec8ac1
AWAIS_SUFI\awais <awais6javaid@gmail.com>
2025-09-05 11:05:25 +0500

    add api

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -7,7 +7,7 @@ module.exports = [
       enabled: true,
       origin: [
         "http://localhost:3000", // local dev
-        "https://your-frontend.com", // production frontend
+        "https://exeonic-service-strapi-lfxn-m54g9uej2-awais-sufis-projects.vercel.app", // production frontend
         "https://www.your-frontend.com", // optional www alias
       ],
       methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
